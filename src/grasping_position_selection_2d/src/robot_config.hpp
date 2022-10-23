d194ee086a4734ed39673a3c23748f22143a26d5
jing_huang <huangjingonly@gmail.com>
2022-10-23 22:38:57 +0800

    2022-10-23

--- src/grasping_position_selection_2d/src/robot_config.hpp
+++ src/grasping_position_selection_2d/src/robot_config.hpp
@@ -20,23 +20,16 @@
 std::string robotIP = "192.168.2.100";
 // IP of the workstation PC running this program
 std::string localIP = "192.168.2.109";
-flexiv::Robot robot(robotIP, localIP);
-flexiv::Scheduler scheduler;
-flexiv::Log robotLog;
-flexiv::RobotStates robotStates;
-
+bool robotInitialized = false;
+flexiv::Robot* robotPtr = nullptr;
 bool direction_1 = true;
 int cnt = 0;
 
 namespace {
-/** Size of Cartesian pose vector [position 3x1 + rotation (quaternion) 4x1 ] */
 const unsigned int k_cartPoseSize = 7;
 static bool isInitPoseSet = false;
 bool isNewTcpPose = false;
-
-/** Initial Cartesian-space pose (position + rotation) of robot TCP */
 static std::vector<double> preTcpPose;
-
 static std::vector<double> targetTcpPose;
 }
 
@@ -65,20 +58,22 @@ void setNewTcpPose(double delta_x, double delta_y) {
     isNewTcpPose = true;
 }
 
+
 /** Callback function for realtime periodic task */
-void periodicTask(flexiv::Robot* robot, flexiv::Scheduler* scheduler,
-    flexiv::Log* log, flexiv::RobotStates& robotStates) {
+void moveRobot(flexiv::Robot* robotPtr) {
     // Flag whether initial Cartesian position is set
+    if (robotPtr == nullptr)
+        return;
 
     try {
         // Monitor fault on robot server
-        if (robot->isFault()) {
+        if (robotPtr->isFault()) {
             throw flexiv::ServerException(
                 "periodicTask: Fault occurred on robot server, exiting ...");
         }
 
-        // Read robot states
-        robot->getRobotStates(robotStates);
+        flexiv::RobotStates robotStates;
+        robotPtr->getRobotStates(robotStates);
 
         // Set initial TCP pose
         if (isInitPoseSet == false) {
@@ -86,105 +81,73 @@ void periodicTask(flexiv::Robot* robot, flexiv::Scheduler* scheduler,
             if (robotStates.tcpPose.size() == k_cartPoseSize) {
                 preTcpPose = robotStates.tcpPose;
                 isInitPoseSet = true;
-                log->info(
+                std::cout <<
                     "Initial Cartesion pose of robot TCP set to [position "
                     "3x1 + rotation (quaternion) 4x1]: "
-                    + flexiv::utility::vec2Str(preTcpPose));
+                    + flexiv::utility::vec2Str(preTcpPose);
             }
         }
         // Run control only after initial pose is set
         else if (isNewTcpPose == true) {
-            robot->streamTcpPose(targetTcpPose);
+            robotPtr->streamTcpPose(targetTcpPose);
+            robotPtr->getRobotStates(robotStates);
             preTcpPose = robotStates.tcpPose;
             isNewTcpPose = false;
         } 
         else {
-            log->error("Unknown motion type");
-            log->info("Accepted motion types: hold, sine-sweep");
-            exit(1);
+            std::cout << "No move is made\n";
         }
 
     } catch (const flexiv::Exception& e) {
-        log->error(e.what());
-        scheduler->stop();
+        std::cerr << e.what();
+        return;
     }
 }
 
 
 int initRobotMain() {
-    // Log object for printing message with timestamp and coloring
-    //flexiv::Log log;
+    flexiv::Log log;
 
-    try {
-        // RDK Initialization
-        //=============================================================================
-        // Instantiate robot interface
-        //robot = flexiv::Robot(robotIP, localIP);
-
-        // Create data struct for storing robot states
-        // flexiv::RobotStates robotStates;
-
-        // Clear fault on robot server if any
+    static flexiv::Robot robot(robotIP, localIP);
+    if (robot.isFault()) {
+        robot.clearFault();
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        // Check again
         if (robot.isFault()) {
-            robotLog.warn("Fault occurred on robot server, trying to clear ...");
-            // Try to clear the fault
-            robot.clearFault();
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-            // Check again
-            if (robot.isFault()) {
-                robotLog.error("Fault cannot be cleared, exiting ...");
-                return 1;
-            }
-            robotLog.info("Fault on robot server is cleared");
+            log.error("Fault cannot be cleared, exiting ...");
+            return 1;
         }
-
-        // Enable the robot, make sure the E-stop is released before enabling
-        robotLog.info("Enabling robot ...");
-        robot.enable();
-
-        // Wait for the robot to become operational
-        int secondsWaited = 0;
-        while (robot.isOperational() == false) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            if (++secondsWaited == 10) {
-                robotLog.warn(
-                    "Still waiting for robot to become operational, please "
-                    "check that the robot 1) has no fault, 2) is booted "
-                    "into Auto mode");
-            }
-        }
-        robotLog.info("Robot is now operational");
-
-        // Set mode after robot is operational
-        robot.setMode(flexiv::MODE_CARTESIAN_IMPEDANCE);
-
-        // Wait for the mode to be switched
-        while (robot.getMode() != flexiv::MODE_CARTESIAN_IMPEDANCE) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-
-        // Choose the index of tool being used. No need to call this method if
-        // the mounted tool on the robot has only one TCP, it'll be used by
-        // default
-        robot.switchTcp(0);
-
-        // Periodic Tasks
-        //=============================================================================
-        //flexiv::Scheduler scheduler;
-        // Add periodic task with 1ms interval and highest applicable priority
-        scheduler.addTask(
-            std::bind(periodicTask, &robot, &scheduler, &robotLog, robotStates),
-            "Servo periodic", 1, 45);
-        // Start all added tasks, this is by default a blocking method
-        scheduler.start();
-
-    } catch (const flexiv::Exception& e) {
-        robotLog.error(e.what());
-        return 1;
+        log.info("Fault on robot server is cleared");
     }
 
-    return 0;
-}
+    log.info("Enabling robot ...");
+    robot.enable();
 
+    // Wait for the robot to become operational
+    int secondsWaited = 0;
+    while (robot.isOperational() == false) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        if (++secondsWaited == 10) {
+            log.warn(
+                "Still waiting for robot to become operational, please "
+                "check that the robot 1) has no fault, 2) is booted "
+                "into Auto mode");
+        }
+    }
+    log.info("Robot is now operational");
+
+    // Set mode after robot is operational
+    robot.setMode(flexiv::MODE_CARTESIAN_IMPEDANCE_NRT);
+
+    // Wait for the mode to be switched
+    while (robot.getMode() != flexiv::MODE_CARTESIAN_IMPEDANCE_NRT) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    robot.switchTcp(0);
+    robotPtr = &robot;
+
+    return 1;
+}
 
 #endif
